b87a10ebb1d2ed5542419a766c258cec0c6cc435
lalaaa2002 <puspadwikomala06@gmail.com>
2019-03-12 11:13:38 +0700

    config changes

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/mur';
+$config['base_url'] = 'http://localhost/sysKP/';
 
 /*
 |--------------------------------------------------------------------------

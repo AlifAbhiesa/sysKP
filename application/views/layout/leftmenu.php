358fa83f00a7461b8dc382eaca59424aa461a7de
lalaaa2002 <puspadwikomala06@gmail.com>
2019-03-16 13:22:55 +0700

    Bimbingan

--- application/views/layout/leftmenu.php
+++ application/views/layout/leftmenu.php
@@ -4,7 +4,7 @@
             <div class="nav-link">
                 <div class="user-wrapper">
                     <div class="profile-image">
-                        <img src="<?php echo base_url()?>assets/images/faces/face1.jpg" alt="profile image">
+                        <img src="<?php echo base_url()?>assets/images/faces/face5.jpg" alt="profile image">
                     </div>
                     <div class="text-wrapper">
                         <p><?php echo $this->session->userdata('username'); ?></p>
@@ -44,6 +44,15 @@
 				<i class="menu-icon fa fa-file" style="color: #3c8dbc"></i> <span>&ensp;Dosen</span>
 			</a>
 		</li>
+
+        <li class="<?php if ($this->uri->segment(1)==='Bimbingan'){
+			echo 'active';
+		}?> nav-item">
+			<a class="nav-link" href="<?php echo base_url()?>Bimbingan" >
+				<i class="menu-icon fa fa-file" style="color: #3c8dbc"></i> <span>&ensp;Bimbingan</span>
+			</a>
+		</li>
+	
 		
 
 		<li class="<?php if ($this->uri->segment(1)==='User'){
---
+++ application/views/pages/page_bimbingan.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: UnixMan
+ * Date: 15/03/2019
+ * Time: 23:13
+ */
+?>
+
+
+<div class="content-wrapper">
+	<div class="col-md-12 grid-margin" id="listUser">
+		<div class="card">
+			<div class="card-body">
+				<div class="box-header">
+					<h3 class="box-title">User List</h3>
+					<hr>
+				</div>
+				<div class="box-body">
+					<hr>
+					<table class="table table-bordered table-striped dt-responsive" id="myData" width="100%" >
+						<thead>
+						<tr>
+							<th>No</th>
+							<th>NRP</th>
+							<th>Nama</th>
+							<th>Judul KP</th>
+							<th>Pembimbing Perusahaan</th>
+							<th>Pembimbing Dosen</th>
+							<th></th>
+						</tr>
+						</thead>
+					</table>
+					<br>
+					<button type="button" class="btn btn-success" style="width: 100%;">
+						Add New Bimbingan
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script>
+	showData()
+	function showData() {
+		//datatables
+		table = $('#myData').DataTable({
+			"columnDefs": [ {
+				"targets": 0,
+				"width": "50px"
+			},
+				{
+					"targets": [1,2,3],
+					"width": "200px"
+				}],
+			"ordering": false,
+			"destroy": true,
+			"serverSide": true,
+			"order": [],
+
+			"ajax": {
+				"url": "<?php echo site_url('Mahasiswa/getAll')?>",
+				"async":false,
+				"type": "POST"
+			},
+
+
+		});
+
+	};
+</script>

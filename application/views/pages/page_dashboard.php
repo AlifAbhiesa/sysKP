466b15cce5694c27c71d5574512b59574071d472
lalaaa2002 <puspadwikomala06@gmail.com>
2019-04-11 09:47:22 +0700

    update dashboard

--- application/views/pages/page_dashboard.php
+++ application/views/pages/page_dashboard.php
@@ -1,28 +1,27 @@
-
 <div class="content-wrapper">
-<div class="row">
-    <div class="col-md-12 grid-margin">
-        <div class="card">
-            <div class="card-body">
-                <div class="row d-none d-sm-flex mb-4">
-                    <div class="col-4">
-                        <h5 class="text-primary">Unique Visitors</h5>
-                        <p>34657</p>
+    <div class="row">
+        <div class="col-md-12 grid-margin">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row d-none d-sm-flex mb-4">
+                        <div class="col-4">
+                            <h5 class="text-primary">Unique Visitors</h5>
+                            <p>34657</p>
+                        </div>
+                        <div class="col-4">
+                            <h5 class="text-primary">Bounce Rate</h5>
+                            <p>45673</p>
+                        </div>
+                        <div class="col-4">
+                            <h5 class="text-primary">Active session</h5>
+                            <p>45673</p>
+                        </div>
                     </div>
-                    <div class="col-4">
-                        <h5 class="text-primary">Bounce Rate</h5>
-                        <p>45673</p>
+                    <div class="chart-container">
+                        <canvas id="dashboard-area-chart" height="80"></canvas>
                     </div>
-                    <div class="col-4">
-                        <h5 class="text-primary">Active session</h5>
-                        <p>45673</p>
-                    </div>
-                </div>
-                <div class="chart-container">
-                    <canvas id="dashboard-area-chart" height="80"></canvas>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </div>
